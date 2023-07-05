ffe571ffbeb61ccfe1e20fc026ac652b6dbd0dae
caiiocorrea <caiosanttos43@gmail.com>
2023-07-05 00:30:44 -0300

    Ajustes para deploy

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -5,4 +5,5 @@ export const environment = {
 		// accessToken: 'pk.eyJ1IjoiY2Fpb2NvcnJlYSIsImEiOiJjbGl4Nnc4OTIwNGFvM2dxeHdjZW96cWtwIn0.HU-PV9BGb9WyrbXA4Qbr-w', //DEV
 		accessToken: 'pk.eyJ1IjoiY2Fpb2NvcnJlYSIsImEiOiJjbGl4NzBmODMwNGF1M2ZvOG9lOGo2czdyIn0.dBTfGoF1klLX4c34h4tXgg', //PROD
 	},
+	pathOrder: 'order',
 };
